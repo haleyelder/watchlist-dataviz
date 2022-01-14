a52f0e7c94069e935ca61b8e17808d4fac1ec2a9
haleyelder <haley.elder@live.com>
2022-01-13 21:01:36 -0800

    updated readme, properly read api response, updates csv

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Watchlist Data Visualization
 
+For most of 2021 and part into 2022, I've been noting down nearly every movie and tv show I have watched. This repo is using python to gather data from OMDB (open movie database API) to fill in some data points and later on use D3 as a data visualization. 
+
+Steps/To Do:
+
+## Python
+[x] - reads csv file; only first row of titles; verify titles are correct/easier to API request later 
+[x] - convert titles to lower case
+[] - convert spaces to + (%20 if needed)
+[x] - request using t=title&key= 
+[] - save data fields to match in csv file; starting example fields below
+    a. Title    
+    b. Release Year
+    c. Genre
+    d. Type (movie or series)
+
+## JavaScript/D3
--- script.py
+++ script.py
@@ -1,56 +1,55 @@
 import csv
 import os
+from platform import release
 import requests
 
 from dotenv import load_dotenv
-
 load_dotenv()
 
-API_ENV_VAR = os.getenv('API_ENV_VAR')
+APIKEY = os.getenv("API_ENV_VAR")
 
-# reads OG watchlist and outputs first row, show/movie titles
-# convert to tower case for search ease?
+# open file and output first line (titles only)
 with open('watchlist.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader: 
         print(row[0].lower())
 
-r = requests.get('https://www.omdbapi.com/?i=tt0167260&apikey=API_ENV_VAR',data={'key': 'value'})
-print(r.text)
+# request IMDB info i=title name
 
+payload = {"apikey":{APIKEY}}
+r = requests.get("https://www.omdbapi.com/?t=Ozark", params=payload)
+response = r.json()
 
-# request info from imdb api
+if r.status_code == 200:
+    rows = []
+    
+    titles = response["Title"]
+    release_year = response["Year"]
 
-# print to new csv file with details input
-# title
-# genre
-# release date 
-# more later
+    # future row titles
+    fields = ["Title", "Years"]
+    
+    print("Titles: " + titles)
+    print("Year: " + release_year)
+    
+    fields = ['Title', 'Years']
+    rows.append([titles])
+    rows.append([release_year])
 
-# example search query with API key https://www.omdbapi.com/?s=Lord%20Of%20The%20Rings&apikey=API_ENV_VAR
+    # print(rows)
 
-# example output; need imdbID 
-# {
-# "Title": "The Lord of the Rings: The Return of the King",
-# "Year": "2003",
-# "imdbID": "tt0167260",
-# "Type": "movie",
-# "Poster": "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-# },
+    # rows = [['LOTR', 1999, 'Fantasy','movie'],
+    #         ['The Great', 2019, 'Drama', 'tv show'],
+    #         ['Belfast', 2021, 'Drama', 'movie'],
+    #         ['The Fall', 2008, 'Fantasy', 'movie']]
 
-# more data with this 
-# https://www.omdbapi.com/?i=tt0167260&apikey=API_ENV_VAR
-# write to csv test
-fields = ['Title', 'Release Year', 'Genre', 'Type']
-rows = [['LOTR', 1999, 'Fantasy','movie'],
-        ['The Great', 2019, 'Drama', 'tv show'],
-        ['Belfast', 2021, 'Drama', 'movie'],
-        ['The Fall', 2008, 'Fantasy', 'movie']]
+    filename = 'watchlist-updated.csv'
 
-filename = 'watchlist-updated.csv'
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
 
-with open(filename, 'w') as csvfile:
-    csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(fields)
+        csvwriter.writerows(rows)
+else: 
+    print('an error has occurred')
 
-    csvwriter.writerow(fields)
-    csvwriter.writerows(rows)
