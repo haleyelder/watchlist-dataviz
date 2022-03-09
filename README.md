4b1dfb8d2fa021249c6343aef4cde351cd84fff0
haleyelder <haley.elder@live.com>
2022-03-08 20:15:38 -0800

    update readme details, partial code work

--- README.md
+++ README.md
@@ -7,14 +7,12 @@ Steps/To Do:
 ## Python
 - [x] reads csv file; only first row of titles; verify titles are correct/easier to API request later 
 - [x] convert titles to lower case
-- [ ] convert spaces to + (%20 if needed)
+- [ ] ~convert spaces to + (%20 if needed)~
 - [x] request using t=title&key= 
-- [ ] save data fields to match in csv file; starting example fields below
-    a. Title    
-    b. Release Year
-    c. Genre
-    d. Type (movie or series)
-- [ ] multiple API requests per line
-- [ ] update singular csv file with all data (completion date is manual)
+- [ ] ~save data fields to match in csv file~
+- [ ] ~multiple API requests per line~
+- [ ] ~update singular csv file with all data (completion date is manual)~
+
+Found a solution through Google Sheets and an API call instead!
 
 ## JavaScript/D3
--- script.py
+++ script.py
@@ -12,28 +12,22 @@ APIKEY = os.getenv("API_ENV_VAR")
 # open file and output first line (titles only)
 with open('watchlist-small.csv', 'r') as file:
     reader = csv.DictReader(file)
+    # save and append listed titles to list
+    # newRow = []
     for row in reader:
-        titles = row["Title"]
-        print(titles)
+        titlesList = row["Title"]
+        # newRow.append(titlesList)
+    # print(newRow)
 
 # request IMDB info t=title name
-
 payload = {"apikey":{APIKEY}}
-r = requests.get("https://www.omdbapi.com/?t=" + titles, params=payload)
+# for titles in newRow:
+#     print(titles)
+r = requests.get("https://www.omdbapi.com/?t=" + titlesList, params=payload)
 response = r.json()
 
 if r.status_code == 200:
     print(response)
-    
-    titles = response["Title"]
-    release_year = response["Year"]
-
-    # future row titles
-    fields = ["Title", "Years"]
-    
-    print("Titles: " + titles)
-    print("Year: " + release_year)
-
     with open('watchlist-updated.csv', 'w', newline='') as csvfile:
         fieldnames = ['Title','Year','Genre','Runtime', 'Language', 'imdbVotes', 'Rated', 'Actors', 'Type', 'Awards', 'Ratings', 'Released', 'Writer', 'imdbRating', 'Poster', 'Director', 'totalSeasons', 'Plot', 'Mpe', 'Awards', 'Ratingsetascore', 'imdbID', 'Response', 'Country','Metascore','Production', 'BoxOffice', 'DVD', 'Website']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
